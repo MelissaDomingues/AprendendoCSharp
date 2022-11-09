bac9dae98486bcb668a174a6d8289b315ef52158
melissa2901 <dominguesriosmel@gmail.com>
2022-11-09 14:47:58 -0300

    ponto.flutuante

---
+++ 3-CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 3 - Variáveis e Ponto Flutuante");
+
+        double salario;
+        salario = 1245.45;
+        Console.WriteLine("O salário naquela empresa é de " + salario);
+
+        Console.WriteLine("Tecle enter para continuar...");
+        Console.ReadLine();
+
+        int divisao = 7/5;
+        Console.WriteLine("Exemplo de divisão usando int: " + divisao);
+
+        double divisao2 = 7.0 / 5;
+        Console.WriteLine("Exemplo de divisão double: " + divisao2);
+        Console.WriteLine("Nos dois exemplos foram usados os mesmos números = 7/5.");
+
+        Console.WriteLine("Tecle enter para encerrar");
+
+        
+
+        
+    }
+
+}
+
