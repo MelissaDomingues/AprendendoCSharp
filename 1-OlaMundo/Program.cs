de82e6ebdeedf61aa7b179e2355a8c863dfbabac
melissa2901 <dominguesriosmel@gmail.com>
2022-11-09 13:36:42 -0300

    Commit inicial

--- 1-OlaMundo/Program.cs
+++ 1-OlaMundo/Program.cs
@@ -4,7 +4,7 @@ class Programa
     static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
-        Console.WriteLine("Tecle enter para fechar...");
+        Console.WriteLine("Tecle enter para fechar!");
         Console.ReadLine();
     }
 
