dec2b8bb069cd13bb138e7e6348d8679b4bcfaaf
Melissa Rios <dominguesriosmel@gmail.com>
2022-11-13 20:09:32 -0300

    AtribuiçõesDeVariáveis

---
+++ 6-AtribuicoesDeVariaveis/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando projeto 6 - Atribuições de variáveis");
+
+        int idade = 30;
+        int idadeAna = idade;
+        Console.WriteLine(idadeAna);
+
+        idade = 25;
+        //nesse caso o print continua sendo 30 enão 25
+        //a leitura é feita de modo linear, então não da pra alterar o valor de uma variável que ja tem um valor (pelo ou menos até agora não rs)
+
+
+
+        Console.WriteLine("Tecle enter para fechar!");
+        Console.ReadLine();
+    }
+
+}
