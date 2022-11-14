6fb3b884a2b7af5897f5a824804976132209d60b
Melissa Rios <dominguesriosmel@gmail.com>
2022-11-13 21:16:45 -0300

    IfAndElse

---
+++ 7-Condicionais/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando projeto  7 - Condicionais");
+
+        string nome = "João";
+        int idade = 22;
+        int acompanhantes = 0;
+
+        if (idade >= 18)
+        {
+            Console.WriteLine(nome + "...Pode entrar. Aproveite a festa!");
+        }
+        else
+        {
+            if (acompanhantes > 0) 
+            {
+                Console.WriteLine(nome + " está acompanhado (a), pode entrar!");
+            }
+            else
+            {
+                Console.WriteLine("Desculpe, mas " + nome + ",não pode entrar!");
+            }
+        }
+          
+        //if e else são comandos do tipo boolean. Só tem um de dois valores possíveis: verdadeiro ou falso.
+
+
+        Console.WriteLine("Tecle enter para fechar!");
+        Console.ReadLine();
+    }
+
+}
