3af4c263924795094ba014e4d5daa337cb35ebc7
Melissa Rios <dominguesriosmel@gmail.com>
2022-11-17 12:20:19 -0300

    9-Escopo

---
+++ 9-Escopo/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando projeto  7 - Condicionais");
+
+        string nome = "João";
+        int idade = 15;
+        int acompanhantes = 1;
+        
+        bool acompanhado = acompanhantes > 0;
+        
+        string textoAdicional;
+         
+        if(acompanhado == true)
+        {
+            textoAdicional = "João está acompanhado";
+        }
+        else
+        {
+            textoAdicional = "João não está acompanhado";
+        }
+
+        if(idade >= 18 || acompanhado)
+        {
+            Console.WriteLine(textoAdicional);
+            Console.WriteLine(nome + "...Pode entrar. Aproveite a festa!");
+        }
+        else
+        {
+            Console.WriteLine(textoAdicional);
+            Console.WriteLine("Desculpe, mas " + nome + ", não pode entrar!");
+        }
+        //o sinal de "==" significa comparação e não atribuição de valor.
+        //Ele verifica se o valor de uma variável é igual a condição estabelecida
+        //bool 0 ou 1 (true or false)
+
+
+
+        Console.WriteLine("Tecle enter para fechar!");
+        Console.ReadLine();
+    }
+
+}
